84b08a8a6f8ba4dbe41cb32f02d954efaa1fea16
John Mark Agosta <johnmark.agosta@gmail.com>
2019-02-06 22:43:36 -0800

    WIP chose_search

--- ml/line_minimization.py
+++ ml/line_minimization.py
@@ -71,6 +71,7 @@ class OneDimOpt:
     def init_grid(self):
         'Create an empty sample. '
         self.converge_flag = True
+        self.widen_attempts = 0
         self.active_min = float(self.range_min)
         self.active_max = float(self.range_max)
         # tuples of (x, f(x)), ordered by x.
@@ -154,106 +155,14 @@ class OneDimOpt:
 
 
     def confidence_bounds(self, X, confidence_factor = 1.0):
-        'For testing if a min exists, or is out of bounds using fit standard errors.'
+        'For testing if a min exists, or is out of bounds using fit standard errors. Returns the std err for a, b, c.'
         inv_mat_diag = np.diag(np.linalg.inv(np.matmul(np.transpose(X),X)))
         inv_mat_diag = [confidence_factor*z for z in inv_mat_diag]
         if self.DBG_LVL > 0:
-            print('Std err a: {:.4}, b: {:.4}, c:{:.4}'.format(*inv_mat_diag))
+            print('Std err c: {:.4}, b: {:.4}, a:{:.4}'.format(*inv_mat_diag))  # Check the order. 
         return inv_mat_diag
 
 
-    def check_fit(self, fit):
-        'Is the fit convex? Is one parabola side much higher than the other?'
-        #TODO -if the points check out to be linear, this corner case should not 
-        #drive the estimated x to -+ inf. 
-        if fit['COEFFICIENTS'][2] < fit['STD_ERR'][2]:
-            if self.DBG_LVL > -1:
-                 print("WARN: Non-convex fit {:.4}: ".format(fit['COEFFICIENTS'][2]), file=sys.stderr)
-        else:
-            return self.OK
-
-    def chose_search(self, fit):
-        'Depending on the fit, either expand, narrow or just increase the sample'
-        # If min lies outside current range, expand to that side.
-        if abs(fit['COEFFICIENTS'][1]) > fit['STD_ERR'][1]: # The slope is significant either way        
-            self.active_min, self.active_max = self.widen_sample()
-            if self.DBG_LVL > -1:
-                 print("Significant slope: {:.4}: ".format(fit['COEFFICIENTS'][1]), file=sys.stderr)
-        # If not convex and not sloped, expand in both directions.
-        # But, fail if expansion doens't help 
-        
-         # If high noise, add sample biased toward extremes
-
-        # If ? V fit, narrow around est min
-
-
-    def run_to_convergence(self, max_iterations):
-        'Assuming min is in search range, iterate to a fixed point'
-        def remove_extreme_pt(pts):
-            'An extreme point is the largest of either the first or last of the sample'
-            # Remove the first point
-            if pts[0][1] > pts[-1][1]:
-                if self.DBG_LVL > -1:
-                    print("Removed ({:.4}, {:.4})".format( pts[0][0], pts[0][1]))
-                del(pts[0])
-                del(self.colors_grid[0])
-                # Reset the interval extent
-                self.active_min = pts[1][0]
-                return pts
-            # Remove the last point
-            else:
-                if self.DBG_LVL > -1:
-                    print("Removed ({:.4}, {:.4})".format( pts[-1][0], pts[-1][1]))
-                del(pts[-1])
-                del(self.colors_grid[-1])
-                # Reset the interval extent
-                self.active_max = pts[-2][0]
-                return pts
-        
-        def low_noise():
-            return False
-                
-        not_converged = True
-        k = 0
-        last_est_min = self.initial_guess
-        while not_converged:
-            print("\nk = {}".format(k))
-            fit = self.fit_parabola_to_sample()
-            self.eval_fit(fit['COEFFICIENTS'])
-            check= self.check_fit(fit) 
-            if check == self.CONCAVE:
-                print('WARN: Try a different starting sample.')
-                self.converge_flag = False
-                break 
-            # Has the search converged?
-            if abs(self.est_min - last_est_min) < self.EPSILON * self.half_range:
-                print('Converged at {:.5}'.format(self.est_min))
-                break
-            if k >= max_iterations:
-                break
-            ### Add a point that straddles the est min, within the active interval
-            #   Just using the current min as the new point, biases the converged value
-            #   to the current estimate, so this sampling approach avoids this
-            #   while still narrowing the sample estimates toward convergence. 
-            #   (Might be worth checking that the current estimate stays in the 
-            #    active interval.)
-            # Check if self.est_min is outside the active interval,
-            # and decide how to expand it. 
-            self.chose_search(fit)
-            if self.DBG_LVL > 0:
-                print("\tActive interval: [{:.4}, {:.4}]".format (self.active_min, self.active_max))
-            # TODO - need a better way to pick points in the interval. e.g SOBOL randomization,
-            # assuming a deterministic function
-            active_x = random.uniform(self.active_min, self.active_max)
-            self.add_f_to_grid(active_x)
-            # and remove an extreme point. But, only if the noise is low.
-            if low_noise():
-            # Successively narrow_sample_to_converge by removing eoints far away.'
-                self.search_grid = remove_extreme_pt(self.search_grid)
-            self.y = [z[1] for z in self.search_grid]
-            last_est_min = self.est_min
-            k += 1
-        return self.search_grid
 
     def eval_fit(self, coeffs):
         'Compute the fitted values for the parabola fit, along with errors and est min.'
@@ -277,20 +186,140 @@ class OneDimOpt:
         self.est_pts = [x_s, y_est]
         return self
 
+
+    def new_sample_pt(self):
+        'Pick a new point in the current active range biased toward influential values.'
+        ### Add a point that straddles the est min, within the active interval
+        #   Just using the current min as the new point, biases the converged value
+        #   to the current estimate, so this sampling approach avoids this
+        #   while still narrowing the sample estimates toward convergence. 
+        #   (Might be worth checking that the current estimate stays in the 
+        #    active interval.)
+        # TODO - need a better way to pick points in the interval. e.g SOBOL randomization,
+        # assuming a deterministic function
+        return  random.uniform(self.active_min, self.active_max)
+        
+
+    def check_fit(self, fit):
+        'Is the fit significantly concave?'
+        #TODO -if the points check out to be linear, this corner case should not 
+        #drive the estimated x to -+ inf. 
+        # A concave function will have a negative curvature; std error of the coef is always positive. 
+        if fit['COEFFICIENTS'][2] < -fit['STD_ERR'][2]:
+            if self.DBG_LVL > -1:
+                 print("WARN: Non-convex fit {:.4}: ".format(fit['COEFFICIENTS'][2]), file=sys.stderr)
+            return self.CONCAVE
+        else:
+            return self.OK
+
+
+    def chose_search(self, fit, max_expansions= 6):
+        'Depending on the fit, either expand, narrow or just increase the sample'
+        # If min lies outside current range, expand to that side.
+        if self.widen_sample(max_expansions):
+            self.widen_attempts += 1
+        if abs(fit['COEFFICIENTS'][1]) > fit['STD_ERR'][1]: # The slope is significant either way        
+            self.widen_symmetric(max_expansions)
+            if self.DBG_LVL > -1:
+                 print("Significant slope: {:.4}: ".format(fit['COEFFICIENTS'][1]), file=sys.stderr)
+        # If convex is not significant and not significant slope, expand in both directions.
+        # If significant slope and not significant convex, expand to that side. 
+        
+         # If high noise, add sample biased toward extremes, and don'e shrink
+
+        # If low noise, shrink extremes. 
+
+    def widen_symmetric(self, max_expansions):
+        'Expand in both directions'
+        pass
+
     def widen_sample(self, max_tries = 6):
         'If the estimated min falls out of the range, adjust the range by doubling it to that side.'
-        if not np.isnan(self.est_min):
-            if self.est_min < self.active_min:
-                self.active_min = max(2 * self.active_min - self.active_max, self.bound_min)
-                if self.DBG_LVL > 0:
-                    print("\tMin reduced to: ", self.active_min)
-            if self.est_min > self.active_max:
-                self.active_max = min(2 * self.active_max - self.active_min, self.bound_max)
-                if self.DBG_LVL > 0:                
-                    print ("\tMax increased to:", self.active_max)
-        return self.active_min, self.active_max 
+        widened = False
+        if self.widen_attempts < max_tries:
+            if not np.isnan(self.est_min):
+                if self.est_min < self.active_min:
+                    self.active_min = max(2 * self.active_min - self.active_max, self.bound_min)
+                    if self.DBG_LVL > 0:
+                        print("\tMin reduced to: ", self.active_min)
+                    widened = True
+                if self.est_min > self.active_max:
+                    self.active_max = min(2 * self.active_max - self.active_min, self.bound_max)
+                    if self.DBG_LVL > 0:                
+                        print ("\tMax increased to:", self.active_max)
+                    widened = True
+
+        else:
+            print("WARN:  Exceeded maximum number of interval widenings.", file = sys.stderr)
+        return widened
 
         
+    def run_to_convergence(self, max_iterations):
+        'Assuming min is in search range, iterate to a fixed point'
+        def remove_extreme_pt(pts):
+            'An extreme point is the largest of either the first or last of the sample'
+            # Remove the first point
+            if pts[0][1] > pts[-1][1]:
+                if self.DBG_LVL > -1:
+                    print("Removed ({:.4}, {:.4})".format( pts[0][0], pts[0][1]))
+                del(pts[0])
+                del(self.colors_grid[0])
+                # Reset the interval extent
+                self.active_min = pts[1][0]
+            # Remove the last point
+            else:
+                if self.DBG_LVL > -1:
+                    print("Removed ({:.4}, {:.4})".format( pts[-1][0], pts[-1][1]))
+                del(pts[-1])
+                del(self.colors_grid[-1])
+                # Reset the interval extent
+                self.active_max = pts[-2][0]
+            self.x = [z[0] for z in pts]
+            self.y = [z[1] for z in pts]
+            return pts
+        
+        def low_noise():
+            return False
+                
+        not_converged = True
+        k = 0
+        last_est_min = self.initial_guess
+        while not_converged:
+            print("\nk = {}".format(k))
+            fit = self.fit_parabola_to_sample()
+            self.eval_fit(fit['COEFFICIENTS'])
+            check= self.check_fit(fit) 
+            if check == self.CONCAVE:
+                print('WARN: Try a different starting sample.')
+                self.converge_flag = False
+                break 
+            # Has the search converged?
+            if abs(self.est_min - last_est_min) < self.EPSILON * self.half_range:
+                print('Converged at {:.5}'.format(self.est_min))
+                self.converge_flag = True
+                break
+            if k >= max_iterations:
+                print('Exceeded max interations {}'.format(k))
+                self.converge_flag = False
+                break
+            # Check self.est_min 
+            # and decide to shrink or to expand it. 
+            self.chose_search(fit)
+            if self.DBG_LVL > 0:
+                print("\tActive interval: [{:.4}, {:.4}]".format (self.active_min, self.active_max))
+            # Increase the sample
+            active_x = self.new_sample_pt()
+            self.add_f_to_grid(active_x)
+            # and remove an extreme point. But, only if the noise is low.
+            if low_noise():
+            # Successively narrow_sample_to_converge by removing eoints far away.'
+                self.search_grid = remove_extreme_pt(self.search_grid)
+
+            last_est_min = self.est_min
+            k += 1
+        return self.search_grid
+
+
     def search_for_min(self, max_iterations = 10, target_function = (lambda x: x*x + x),
             initial_sample =5):
         # Create some widely-spaced starting points, to broaden search over possible local optima. 
@@ -320,6 +349,7 @@ def plot_search_grid(search_grid,
     return p
     #show(p)
 
+
 def plot_residuals(residuals):
     p = figure(plot_width = 600, plot_height = 600, 
         title = 'Convergence Path',
@@ -334,6 +364,8 @@ def plot_residuals(residuals):
     return p
     #show(p)
 
+###################################################################################
+# Test function examples
 def v_func(x, lft =10, rht =4, noise = 0.0):
     min_pt = 1
     kcenter = 0
@@ -347,7 +379,7 @@ def v_func(x, lft =10, rht =4, noise = 0.0):
     return fx 
 
 # A min outside the search range
-def almost_lin( x, b = 0, a = 3.01, noise = 4.0):
+def almost_lin( x, b = 0, a = -0.01, noise = 1.0):
     return 1 + b*x + a*x*x + noise*np.random.random_sample()
     
 # A decidedly non-parabolic function with a global min 
